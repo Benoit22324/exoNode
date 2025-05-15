957fa254418dda0a898487504b68473923d6649f
Benoit22324 <sunbenoit384@gmail.com>
2025-05-15 11:38:53 +0200

    Ajout exo pug

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 import pug from "pug";
 import path from "node:path";
 
-pug.renderFile(path.join("src", "view", "admin.pug"), { isAdmin: true }, (err, html) => {
-    console.log(html)
-});
+const loggedUser = {
+    name: {
+        first: 'Jean',
+        last: 'Dupont',
+    },
+    age: 36,
+    birthdate: new Date('1986-04-18'),
+    location: {
+        zipcode: '77420',
+        city: 'Champs-sur-Marne',
+    },
+    isAdmin: true
+};
+
+const compile = pug.compileFile(path.join("src", "view", "user.pug"), {pretty: true});
+console.log(compile(loggedUser))
+
+// pug.renderFile(path.join("src", "view", "admin.pug"), { isAdmin: true }, (err, html) => {
+//     console.log(html)
+// });
