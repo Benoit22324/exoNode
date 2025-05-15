11c3f12609034611a6644346357abc4fd670043d
Benoit22324 <sunbenoit384@gmail.com>
2025-05-15 14:51:54 +0200

    Ajout exo pug layout

--- src/app.js
+++ src/app.js
@@ -1,23 +1,12 @@
 import pug from "pug";
 import path from "node:path";
 
-const loggedUser = {
-    name: {
-        first: 'Jean',
-        last: 'Dupont',
-    },
-    age: 36,
-    birthdate: new Date('1986-04-18'),
-    location: {
-        zipcode: '77420',
-        city: 'Champs-sur-Marne',
-    },
-    isAdmin: true
-};
+const menuItems = [
+    { path: '/', title: 'Home', isActive: true },
+    { path: '/about-me', title: 'About', isActive: false },
+    { path: '/references', title: 'References', isActive: false },
+    { path: '/contact-me', title: 'Contact', isActive: false },
+];
 
-const compile = pug.compileFile(path.join("src", "view", "user.pug"), {pretty: true});
-console.log(compile(loggedUser))
-
-// pug.renderFile(path.join("src", "view", "admin.pug"), { isAdmin: true }, (err, html) => {
-//     console.log(html)
-// });
+const compile = pug.compileFile(path.join("src", "view", "home.pug"), {pretty: true})
+console.log(compile({menuItems}))
